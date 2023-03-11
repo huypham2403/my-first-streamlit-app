a24e11e58398cfa4e993bbe2e2f0eb9867a71638
Phạm Lê Tường Huy <124581563+huypham2403@users.noreply.github.com>
2023-03-11 14:46:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ st.title('GIẢI PHƯƠNG TRÌNH BẬC NHẤT')
 a = st.number_input('Tham số a')
 b = st.number_input('Tham số b')
 if st.button('Giải'):
-    if a != 0:
-        result = -b/a
-        st.success(f'Phương trình có một nghiệm {result}')
-    else:
+    if a == 0:
+        if b==0:
+            st.success(f'Phương trình vô nghiệm')
         st.success(f'Phương trình vô nghiệm')
+    else: 
+        result = -b/a
+        st.warning(f'Phương trình có một nghiệm {result}')
